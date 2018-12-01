d5c31cf29093bb6135d039b320cb07c0132248ba
Arkasia <amaury.doudement@gmail.com>
2018-12-01 19:42:25 +0100

    Adding the game window

--- src/main/com/wemanity/kata/tdd/bejeweledlike/Main.java
+++ src/main/com/wemanity/kata/tdd/bejeweledlike/Main.java
@@ -1,9 +1,11 @@
 package com.wemanity.kata.tdd.bejeweledlike;
 
 
+import com.wemanity.kata.tdd.bejeweledlike.view.Window;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        new Window().create();
     }
 }
